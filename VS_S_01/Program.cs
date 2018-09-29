69a469389be6e8212ed95f5a4d77b5b6a03794b9
Mehdi Khodaeifard <Mehdi Khodaeifard@WIN10-VM-MAC>
2018-09-29 16:43:54 +0200

    Add project files.

---
+++ VS_S_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_S_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
